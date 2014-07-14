26f1ba398f54264239621c3e0a8b083cb7905943
minshik <bucketzxm@gmail.com>
2014-07-14 22:19:49 +0800

    test

--- revcpp.py
+++ revcpp.py
@@ -8,7 +8,9 @@ def clean(rootdir):
     #print("In dir: "+rootdir)
     for dirpath,dirname,filename in os.walk(rootdir):
         for file in filename:
-            if file[-3:] == "exe" or file[-1:] == "o" or '.' not in file:
+            if file=='.git':
+                continue
+	    if file[-3:] == "exe" or file[-1:] == "o" or '.' not in file:
                 print("removed" + rootdir+"/"+file)
                 os.remove(rootdir+"/"+file)
         for dirp in dirname:
