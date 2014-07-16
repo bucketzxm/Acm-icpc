075f3c0b3e28d949ae95fa51760cb5e5877ec96a
minshik <bucketzxm@gmail.com>
2014-07-16 14:15:03 +0800

    2014716

---
+++ hdu/线段树/2795.cpp
@@ -0,0 +1,109 @@
+
+#include <iostream>
+#include <sstream>
+#include <ios>
+#include <iomanip>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cctype>
+using namespace std;
+#define XINF INT_MAX
+#define INF 0x3FFFFFFF
+#define MP(X,Y) make_pair(X,Y)
+#define PB(X) push_back(X)
+#define REP(X,N) for(int X=0;X<N;X++)
+#define REP2(X,L,R) for(int X=L;X<=R;X++)
+#define DEP(X,R,L) for(int X=R;X>=L;X--)
+#define CLR(A,X) memset(A,X,sizeof(A))
+#define IT iterator
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef vector<PII> VII;
+typedef vector<int> VI;
+#define MAXN 200010
+
+struct T{
+    int left , right ;
+    int num;
+};
+T tree[5*MAXN];
+int h,w,n;
+
+void init(int o,int l,int r)
+{
+    tree[o].num = w;
+    int mid = (l+r)/2;
+
+    tree[o].left = l;
+    tree[o].right = r;
+
+    if( l==r)return;
+    init(o*2+1,l,mid);
+    init(o*2+2,mid+1,r);
+    return;
+}
+
+
+int ans ;
+int query(int l,int r,int o,int tw)
+{
+    if( l == r)
+    {
+        tree[o].num-=tw;
+        ans = l;
+        return tree[o].num;
+    }
+
+    int mid = (l+r)/2;
+
+    if( tree[2*o+1].num >= tw)
+    {
+        tree[o].num = max( query(l,mid,2*o+1,tw), tree[2*o+2].num);
+    }
+    else
+    {
+        tree[o].num = max( query(mid+1,r,2*o+2,tw),tree[o*2+1].num);
+    }
+    return tree[o].num;
+}
+
+int main()
+{
+//	ios::sync_with_stdio(false);
+    #ifdef LOCAL
+        freopen("in","r",stdin);
+    #endif
+
+    while(scanf("%d%d%d",&h,&w,&n)!=EOF)
+    {
+        h = min(h,n);
+        init(0,1,h);
+
+        REP(i,n)
+        {
+            int tw;
+            scanf("%d",&tw);
+            if( tw > tree[0].num){
+                printf("-1\n");
+                continue;
+            }
+            query(1,h,0,tw);
+            printf("%d\n",ans);
+        }
+    }
+
+	return 0;
+}
---
+++ poj/线段树/3468.cpp
@@ -0,0 +1,154 @@
+
+#include <iostream>
+#include <sstream>
+#include <ios>
+#include <iomanip>
+#include <functional>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <list>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <cstdio>
+#include <cstdlib>
+#include <cmath>
+#include <cstring>
+#include <climits>
+#include <cctype>
+using namespace std;
+#define XINF INT_MAX
+#define INF 0x3FFFFFFF
+#define MP(X,Y) make_pair(X,Y)
+#define PB(X) push_back(X)
+#define REP(X,N) for(int X=0;X<N;X++)
+#define REP2(X,L,R) for(int X=L;X<=R;X++)
+#define DEP(X,R,L) for(int X=R;X>=L;X--)
+#define CLR(A,X) memset(A,X,sizeof(A))
+#define IT iterator
+typedef long long ll;
+typedef pair<int,int> PII;
+typedef vector<PII> VII;
+typedef vector<int> VI;
+
+#define MAXN 200000
+struct node
+{
+    ll num;
+    ll sum;
+    int left,right;
+    node* lson,*rson;
+    node():num(0),sum(0),left(0),right(0),lson(NULL),rson(NULL){}
+};
+
+node tree[MAXN*4];
+
+int N,Q;
+
+void build(node* p ,int l,int r)
+{
+
+    p->left = l ;
+    p->right = r;
+
+    if( l==r) return;
+    int mid = (l+r)/2;
+
+    p->lson = tree+2*(p-tree)+1;
+    p->rson = tree+2*(p-tree)+2;
+    build(p->lson,l,mid);
+    build(p->rson,mid+1,r);
+}
+
+void add(node* p,int l,int r,int t)
+{
+    p->sum += t *(r-l+1);
+
+    if( p->left == l && p->right ==r)
+    {
+        p->num += t;
+        return;
+    }
+
+    int mid = (p->left + p->right)/2;
+
+    if( l > mid)
+    {
+        add( p->rson,l,r,t);
+    }
+    else if( r <= mid)
+    {
+        add ( p->lson,l,r,t);
+    }
+    else
+    {
+        add( p->lson,l,mid,t);
+        add(p->rson,mid+1,r,t);
+    }
+    return ;
+}
+
+ll query( node* p,int l,int r)
+{
+  //  cout<<p->left<<" "<<p->right<<endl;
+    ll ret = p->num*(r-l+1);
+
+    if( l== p->left && r==p->right)
+    {
+        return p->sum;
+    }
+
+    int m = (p->left + p->right)/2;
+
+    if( l>m)
+    {
+        return ret + query( p->rson,l,r);
+    }
+    else if( r<= m)
+    {
+        return ret + query( p->lson,l,r);
+    }
+    else
+    {
+        return ret+ query(p->lson ,l,m) + query( p->rson,m+1,r);
+    }
+}
+
+int main()
+{
+	//ios::sync_with_stdio(false);
+    #ifdef LOCAL
+        freopen("in","r",stdin);
+    #endif
+
+    while(cin>>N>>Q)
+    {
+        build(tree,1,100000);
+        for(int i=1;i<=N;i++)
+        {
+            int t;
+            scanf("%d",&t);
+            add(tree,i,i,t);
+        }
+        for(int i=0;i<Q;i++)
+        {
+            getchar();
+            char cmd;int x,y;
+            scanf("%c %d %d",&cmd,&x,&y);
+            if( cmd== 'Q')
+            {
+                printf("%lld\n",query(tree,x,y));
+            }
+            else
+            {
+                int t;
+                scanf("%d",&t);
+                add(tree,x,y,t);
+            }
+        }
+    }
+	return 0;
+}
